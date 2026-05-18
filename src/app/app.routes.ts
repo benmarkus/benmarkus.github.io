bdcf53876c4c5e1613cffd98efb465c7ea972378
benmarkus <markusbence0@gmail.com>
2026-05-18 23:28:51 +0200

    wire data to projects page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProjectsPage } from './features/projects/projects-page/projects-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'projects', component: ProjectsPage },
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
+];
--- src/app/features/projects/projects-page/projects-page.ts
+++ src/app/features/projects/projects-page/projects-page.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ProjectCard } from '../project-card/project-card';
+import { Project } from '../project.model';
+import projectsData from '../../../../data/projects.json';
 
 @Component({
   selector: 'app-projects-page',
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './projects-page.html',
   styleUrl: './projects-page.css',
 })
-export class ProjectsPage {}
+export class ProjectsPage {
+  projects: Project[] = projectsData;
+}
