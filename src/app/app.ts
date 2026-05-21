da8bb2caaffa7290a44674eab2d7fee1e335c261
benmarkus <markusbence0@gmail.com>
2026-05-21 13:07:21 +0200

    create header and navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './features/layout/navbar/navbar';
+import { Header } from './features/layout/header/header';
 import { Footer } from './features/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
