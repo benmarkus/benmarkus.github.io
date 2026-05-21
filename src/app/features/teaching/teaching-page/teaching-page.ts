d49aa1d0d6f1052c4c8207ab265ae7bc7b1c6be8
benmarkus <markusbence0@gmail.com>
2026-05-21 13:16:26 +0200

    add further main pages

---
+++ src/app/features/teaching/teaching-page/teaching-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teaching-page',
+  imports: [],
+  templateUrl: './teaching-page.html',
+  styleUrl: './teaching-page.css',
+})
+export class TeachingPage {}
