c8899bcbc59ab83c685e8121b02b6bf166561cdd
benmarkus <markusbence0@gmail.com>
2026-08-30 19:13:00 +0200

    remove unused home and cv pages and increase navbar element fontsize

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './features/home/home-page/home-page';
 import { ProjectsPage } from './features/projects/projects-page/projects-page';
 import { TeachingPage } from './features/teaching/teaching-page/teaching-page';
-import { CvPage } from './features/cv/cv-page/cv-page';
 
 export const routes: Routes = [
-  { path: 'home', component: HomePage },
   { path: 'projects', component: ProjectsPage },
   { path: 'teaching', component: TeachingPage },
-  { path: 'cv', component: CvPage },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
 ];
